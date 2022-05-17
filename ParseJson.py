ed67190b2c54bf5e6eca24a839cce36e5de48e88
flacogabrielc <flacogabrielc@hotmail.com>
2022-05-17 17:32:37 -0300

    solo una prueba

--- ParseJson.py
+++ ParseJson.py
@@ -4,3 +4,6 @@ odics = '{"K1":"val1","K2":"val2"}'
 json_result = json.loads(odics)
 print(json_result['K1'])
 print(json_result)
+
+
+--git remote add origin https://github.com/NOMBRE_USUARIO/NOMBRE_PROYECTO.git
