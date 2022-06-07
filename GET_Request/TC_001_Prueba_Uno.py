7a0fd169e7b11367a086712d0889dbc1a683c4ee
flacogabrielc <flacogabrielc@hotmail.com>
2022-06-07 16:06:42 -0300

    Se empiezan a escribir los casos en formato TEST

---
+++ GET_Request/TC_001_Prueba_Uno.py
@@ -0,0 +1,8 @@
+#Test case code must be written inside a method
+
+def test_tc_001_get_users():
+    #put test case code over there
+    #method with test name
+    print('This is out test case code')
+    print('This is end of my test case')
+
