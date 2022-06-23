ab894fc4822e594449299f5f514a00865594d155
flacogabrielc <flacogabrielc@hotmail.com>
2022-06-23 11:05:08 -0300

    01-Alta Cliente

---
+++ Fase I/test_TC_001.py
@@ -0,0 +1,19 @@
+import pytest
+import json
+import requests
+
+def test_tc_001_alta_cliente():
+    headersdata = {'Content-Type': 'application/json', 'apikey': 'ltYkkzeoPZLhYtXjNpYpTt9cEFb9elNE'}
+
+    # data is the body
+    data = {"externalId": "544c866c-4f36-44c2-aece-b8faafe470eb"
+
+    }
+
+    url = "https://api.qa.clave.cloud/gateway/customers"
+
+    response = requests.post(url, headers=headersdata, json=data)
+    response_json = response.json()
+    print(response_json)
+
+#def test_tc_002_cons_pro_alta():
